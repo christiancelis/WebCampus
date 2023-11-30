902dcbc603512d30edc7206aa366c46ec4079c0e
ChristianCelis <cristian_celis10@hotmai.com>
2023-11-29 21:41:40 -0500

    aa

---
+++ codigoPruebaJS/pruebajs.js
@@ -0,0 +1,14 @@
+a = Number(prompt("Digite un numero"))
+b= Number(prompt("Digite otro numero"))
+
+suma = a+b
+resta = a-b
+multiplicacion = a*b
+division = a/b
+exponente = a**b
+
+
+resultado = `${suma} \n ${resta}`
+console.log(resultado)
+alert(resultado)
+
